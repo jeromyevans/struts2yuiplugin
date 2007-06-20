bac6345ed251daeadc18bc39bc43edd6a1991a24
musachy <musachy@98e1cd0d-3733-0410-ace0-b936c0980338>
2007-06-20 16:41:51 +0000

    Add i18n support

---
+++ trunk/src/main/resources/org/apache/struts2/static/i18n/fr.js
@@ -0,0 +1,10 @@
+YAHOO.s2yui.i18n.fr = {
+    //months-format-wide
+    months_long : ["Janvier", "F\xe9vrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Ao\xfbt", "Septembre", "Octobre", "Novembre", "D\xe9cembre"],
+    //months-format-abbr
+    months_short: ["Janv", "F\xe9vr", "Mars", "Avr", "Mai", "Juin", "Juil", "Ao\xfbt", "Sept", "Oct", "Nov", "D\xe9c"], 
+    //days-format-wide
+    weekdays_long: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+    //days-format-abbr
+    weekdays_short: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
+};
---
+++ trunk/src/main/resources/org/apache/struts2/static/i18n/it.js
@@ -0,0 +1,10 @@
+YAHOO.s2yui.i18n.it = {
+    //months-format-wide
+    months_long : ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+    //months-format-abbr
+    months_short: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"], 
+    //days-format-wide
+    weekdays_long: ["Domenica", "Luned\xec", "Marted\xec", "Mercoled\xec", "Gioved\xec", "Venerd\xec", "Sabato"],
+    //days-format-abbr
+    weekdays_short: ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"]
+};
